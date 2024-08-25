91133ee3a35b1fb8b7a05b35db9add22a4001ac1
Santiago Gonzalez Angarita <sega97@gmail.com>
2024-08-25 16:14:16 -0500

    clean

--- clean_body_data.py
+++ clean_body_data.py
@@ -1,35 +1,24 @@
 import pandas as pd
 
 def clean_body_data(input_filename='body_data.csv', output_filename='clean_body_data.csv'):
-    # Load the data
-    df = pd.read_csv(input_filename)
+    
+    df = pd.read_csv(input_filename)                        # Load the data
 
     pd.set_option('future.no_silent_downcasting', True)
+    df = df.infer_objects()                                 # Infer object dtypes if necessary
 
-    # Infer object dtypes if necessary
-    df = df.infer_objects()
+    df = df.drop_duplicates(subset='date', keep='first')    # Drop duplicate dates, keeping the first occurrence
 
-    # Drop duplicate dates, keeping the first occurrence
-    df = df.drop_duplicates(subset='date', keep='first')
+    df['date'] = pd.to_datetime(df['date'])                 # Ensure datetime
+    df = df.sort_values(by='date')                          # Sort the DataFrame by date to ensure correct forward filling
 
-    # Sort the DataFrame by date to ensure correct forward filling
-    df['date'] = pd.to_datetime(df['date'])
-    df = df.sort_values(by='date')
+    
+    df[['bmi', 'fat', 'weight']] = df[['bmi', 'fat', 'weight']].apply(pd.to_numeric, errors='coerce')   # Convert columns to numeric, forcing errors to NaN\
+    df[['bmi', 'fat', 'weight']] = df[['bmi', 'fat', 'weight']].replace(0, pd.NA)                       # Replace zeros with NaN
+    df[['bmi', 'fat', 'weight']] = df[['bmi', 'fat', 'weight']].ffill()                                 # Forward fill missing values
 
-    # Convert columns to numeric, forcing errors to NaN
-    df[['bmi', 'fat', 'weight']] = df[['bmi', 'fat', 'weight']].apply(pd.to_numeric, errors='coerce')
-
-    # Replace zeros with NaN
-    df[['bmi', 'fat', 'weight']] = df[['bmi', 'fat', 'weight']].replace(0, pd.NA)
-
-    # Forward fill missing values
-    df[['bmi', 'fat', 'weight']] = df[['bmi', 'fat', 'weight']].ffill()
-
-    # Save the cleaned data to a new CSV file
-    df.to_csv(output_filename, index=False)
-
-    # Display final state of the DataFrame
-    print(f"Cleaned DataFrame saved to {output_filename}:")
+    df.to_csv(output_filename, index=False)                 # Save the cleaned data to a new CSV file
+    print(f"Cleaned DataFrame saved to {output_filename}:") # Display final state of the DataFrame
 
 if __name__ == "__main__":
     clean_body_data()
